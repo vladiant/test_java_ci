c4d299cc045b3ad2ffda65c66c46e7e5f2d5d825
Vladislav Antonov <vladislav.antonov@gmail.com>
2026-06-27 22:07:34 +0300

    Fix CI error

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     jacoco
     checkstyle
     id("com.github.spotbugs") version "6.5.8"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.gradleup.shadow") version "9.4.3"
 }
 
 group = "com.example"
